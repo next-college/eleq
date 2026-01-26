3fb99385562f9eb794a70dad890297bea7eef89f
Jahakeme <jucalasitok@gmail.com>
2026-01-26 13:42:21 +0100

    feat: add failed order page

---
+++ frontend/app/orders/failed/page.tsx
@@ -0,0 +1,9 @@
+import { FailedPage } from "@/components/checkout/FailedPage";
+
+export default function OrderFailedPage() {
+  return (
+    <div className="min-h-screen bg-(--colour-background) flex items-center justify-center p-4">
+      <FailedPage />
+    </div>
+  );
+}
